532a9e7b80a6bcd269c133b33c4718f215a7b9d1
Patryk <patryk080998@gmail.com>
2020-03-21 19:54:54 +0100

    forgot to push

--- src/mygamewishlist/model/dao/mapper/WishListGameMapper.java
+++ src/mygamewishlist/model/dao/mapper/WishListGameMapper.java
@@ -1,6 +1,14 @@
 package mygamewishlist.model.dao.mapper;
 
-public interface ListMapper {
+import java.util.ArrayList;
 
+import org.apache.ibatis.annotations.Param;
+
+import mygamewishlist.model.pojo.db.WishListGame;
+
+public interface WishListGameMapper {
+
+	public ArrayList<WishListGame> getListByIdUser(@Param("idUser") int idUser);
 	
+	public WishListGame getGameFromListByIdUser(@Param("idUser") int idUser, @Param("url") String url);
 }
